9f60618ec3caeee3257cc17da51fb9b08ca121cc
Walid Wazini <wazini25@gmail.com>
2022-04-08 10:21:34 +0800

    api routes and connecting MongoDb

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,32 @@
+// api/new-meetup
+import { MongoClient } from 'mongodb'
+
+const databaseName = 'meetups'
+const username = 'walid'
+const userPassword = ''
+
+const handler = async (req, res) => {
+  if (req.method === 'POST') {
+    const data = req.body
+
+    // const { title, image, address, description } = data
+
+    // Store into database
+    const client = await MongoClient.connect(`mongodb+srv://${username}:${userPassword}@meetup-places.xjzy3.mongodb.net/${databaseName}?retryWrites=true&w=majority`)
+    const db = client.db()
+
+    const meetupsCollection = db.collection('collection-1')
+
+    const result = await meetupsCollection.insertOne(data)
+
+    console.log(result)
+
+    client.close()
+
+    res.status(2001).json({
+      message: 'Meetup inserted'
+    })
+  }
+}
+
+export default handler 
